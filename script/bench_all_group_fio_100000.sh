174827ea7d771719653112749847934b63f7ba44
Komei Kamiya <kamiya@hpcs.cs.tsukuba.ac.jp>
2014-10-11 01:20:23 +0900

    * ベンチマーク用のスクリプトファイルを追加

---
+++ script/bench_all_group_fio_100000.sh
@@ -0,0 +1,10 @@
+for i in `seq 1 10`
+do
+    g++ -g -O2 -std=c++0x -lpthread -o ../queue_mgr.o -c -DBATCH_TEST -DGROUP_COMMIT_NUM=${i} ../queue_mgr.cpp
+    g++ -g -O2 -std=c++0x -lpthread -o ../aries_fio.exe ../main.o ../transaction.o ../system.o ../logger.o ../update.o ../global.o ../buffer.o ../queue_mgr.o
+    ./bench_fio_batch_100000.sh
+    mv fio.csv fio_batch_group${i}.csv
+done
+
+
+
