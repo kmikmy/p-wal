0095eeaa400f3bea48b16db3a000cf6a3a1ba280
Komei Kamiya <kamiya@hpcs.cs.tsukuba.ac.jp>
2014-10-14 02:10:47 +0900

    * scriptファイルの修正 - ベンチマーク結果がおかしくなっていたのは、適切にNUM_GROUP_COMMITが定義されていなかったから

--- script/bench_all_group_fio_10000.sh
+++ script/bench_all_group_fio_10000.sh
@@ -1,6 +1,6 @@
 for i in `seq 1 10`
 do
-    g++ -g -O2 -std=c++0x -lpthread -o ../queue_mgr.o -c -DBATCH_TEST -DGROUP_COMMIT_NUM=${i} ../queue_mgr.cpp
+    g++ -g -O2 -std=c++0x -lpthread -o ../queue_mgr.o -c -DBATCH_TEST -DNUM_GROUP_COMMIT=${i} ../queue_mgr.cpp
     g++ -g -O2 -std=c++0x -lpthread -o ../aries_fio.exe ../main.o ../transaction.o ../system.o ../logger.o ../update.o ../global.o ../buffer.o ../queue_mgr.o
     ./bench_fio_batch_10000.sh
     mv fio.csv fio_batch_group${i}.csv
