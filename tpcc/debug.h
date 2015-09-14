49139843e11f3faf32a23014e9b8d4fc85647855
Komei Kamiya <kamiya@hpcs.cs.tsukuba.ac.jp>
2015-09-15 01:48:31 +0900

    * 不要なファイルを削除した

--- tpcc/debug.h
+++
@@ -1,8 +0,0 @@
-#ifndef _debug
-#define _debug
-
-#define NNN       do {fprintf(stderr, "%ld %16s %4d %16s\n", (long int)pthread_self(), __FILE__, __LINE__, __func__); fflush(stderr);} while (0)
-#define ERR       do {perror("ERROR"); NNN; exit(1);} while (0)
-#define PERR(val) do {perror(val); NNN; exit(1);} while (0)
-
-#endif
